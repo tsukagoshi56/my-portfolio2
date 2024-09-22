08d7eac945b0b0ac9b2b5dc146cbecb59e5c904a
tsukagoshi56 <tsukatoshi0506@gmail.com>
2024-09-22 14:12:34 +0900

    Remove Contact component and related links

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
-import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
 export default function Home() {
@@ -23,7 +22,6 @@ export default function Home() {
         <About />
         <Projects />
         <Skills />
-        <Contact />
       </main>
       <Footer />
     </div>
